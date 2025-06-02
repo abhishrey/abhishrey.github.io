13aafc3ddc7bac7277576c4b1c58114cff7af898
abhishreya <abhishreya.sharma@ovgu.de>
2025-06-02 18:11:00 +0200

    improved UI of exp

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -14,44 +14,53 @@ function Experience() {
         <div className="relative border-l-4 border-blue-400 pl-4 sm:pl-8">
           {/* Job 1 */}
           <div className="mb-12 group">
-            <div className="bg-white p-8 rounded-xl shadow-lg border-l-4 border-blue-200 group-hover:shadow-xl transition">
+            <div className="bg-white p-8 rounded-xl shadow-lg border-l-4 border-blue-200 group-hover:shadow-xl transition transform hover:scale-105 hover:shadow-xl duration-300">
               <h3 className="font-semibold text-xl">WerkStudent: Robert Bosch</h3>
               <p className="text-gray-600">10/2023 - Present</p>
               <p className="text-gray-600">Renningen, Germany</p>
-              <ul className="list-disc pl-5 mt-3 text-gray-800">
+              <ul className="text-gray-700 text-sm mt-4 list-disc list-outside pl-5 space-y-1">
                 <li>Developed a custom GitHub Action to cache and restore project dependencies, reducing build and deployment time by 50%.</li>
                 <li>Leveraged software development skills to design and implement a web app for managing GitHub repositories, optimizing deployment through CI/CD pipelines.</li>
                 <li>Designed and developed a Streamlit web app for a Machine Learning classifier.</li>
                 <li>Prototyped a React JS-based annotation tool for image region selection.</li>
                 <li>Ensured software reliability through comprehensive unit and system testing.</li>
               </ul>
+              <div className="text-blue-600 text-sm mt-4 block">
+                Tech stack: GitHub Actions | Python | Streamlit | React | CI/CD | Docker | Unit Testing | REST | GraphQL | JIRA | Agile | GIT
+              </div>
             </div>
           </div>
 
           {/* Job 2 */}
           <div className="mb-12 group">
-            <div className="bg-white p-8 rounded-xl shadow-lg border-l-4 border-blue-200 group-hover:shadow-xl transition">
+            <div className="bg-white p-8 rounded-xl shadow-lg border-l-4 border-blue-200 group-hover:shadow-xl transition transform hover:scale-105 hover:shadow-xl duration-300">
               <h3 className="font-semibold text-xl">Research Assistant: Otto-von-Guericke-University - AI LAB</h3>
               <p className="text-gray-600">03/2023 - 12/2023</p>
               <p className="text-gray-600">Magdeburg, Germany</p>
-              <ul className="list-disc pl-5 mt-3 text-gray-800">
+              <ul className="text-gray-700 text-sm mt-4 list-disc list-outside pl-5 space-y-1">
                 <li>Designed and developed a React JS-based user study toolbox to load audio waveforms and conduct seamless annotations.</li>
                 <li>Improved the user interface of an existing web app to enhance usability and aesthetics.</li>
               </ul>
+              <div className="text-blue-600 text-sm mt-4 block">
+                Tech stack: React | HTML5 | Tailwind | JavaScript | UI/UX | GitLab
+              </div>
             </div>
           </div>
 
           {/* Job 3 */}
           <div className="mb-12 group">
-            <div className="bg-white p-8 rounded-xl shadow-lg border-l-4 border-blue-200 group-hover:shadow-xl transition">
+            <div className="bg-white p-8 rounded-xl shadow-lg border-l-4 border-blue-200 group-hover:shadow-xl transition transform hover:scale-105 hover:shadow-xl duration-300">
               <h3 className="font-semibold text-xl">Senior Systems Engineer: Infosys</h3>
               <p className="text-gray-600">11/2019 - 08/2022</p>
               <p className="text-gray-600">Bangalore, India</p>
-              <ul className="list-disc pl-5 mt-3 text-gray-800">
+              <ul className="text-gray-700 text-sm mt-4 list-disc list-outside pl-5 space-y-1">
                 <li>Implemented and maintained web applications for the Indian government’s tax portal, using Angular, AngularJS, TypeScript, JavaScript, HTML5, CSS3.</li>
                 <li>Managed database interaction with MySQL and integrated RESTful Web Services, ensuring secure and efficient data handling.</li>
                 <li>Participated in all phases of project development, collaborating with cross-functional teams in Agile workflows to gather requirements, address bugs, and manage deployments using JIRA.</li>
               </ul>
+              <div className="text-blue-600 text-sm mt-4 block">
+                Tech stack: Angular | AngularJS | TypeScript | JavaScript | HTML5 | CSS3 | MySQL | REST | JIRA | Agile | SVN
+              </div>
             </div>
           </div>
         </div>
